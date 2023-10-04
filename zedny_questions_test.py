18e0bee8041dd8694b0f929e4c89a734b034f0a8
eslam khaled <112361119+eslamkhaled88@users.noreply.github.com>
2023-10-04 17:00:29 +0300

    Update zedny_questions_test.py

--- zedny_questions_test.py
+++ zedny_questions_test.py
@@ -1,31 +1,19 @@
+# %%writefile zedny_questions.py
+
 import streamlit as st
 import pandas as pd
 import os
-import openpyxl
-# Define the CSS for styling with text-align set to "right" and custom input styling
-app_alignment_css = """
-<style>
-    body {
-        direction: rtl;
-    }
-    input[type="text"] {
-        width: max-content;
-        margin-right: 5px;  /* Adjust the margin as needed */
-        margin-left: 5px;   /* Adjust the margin as needed */
-    }
-</style>
-"""
 
-# Apply the CSS style to the entire app
-st.markdown(app_alignment_css, unsafe_allow_html=True)
+# Create a Streamlit app
+st.title("Zedny Questions Editor")
 
 # Page for uploading the Excel file
 def upload_page():
-    st.header("رفع ملف الإكسل")
+    st.header("Upload Excel File")
     st.write("Please upload an Excel file containing questions and answers.")
     
     # Add a file uploader for Excel file with a unique key
-    uploaded_file = st.file_uploader("رفع ملف الإكسل", key="file_uploader", type=["xls", "xlsx"])
+    uploaded_file = st.file_uploader("Upload an Excel file", key="file_uploader", type=["xls", "xlsx"])
     
     if uploaded_file is not None:
         # Save the uploaded file to a temporary location
@@ -40,18 +28,17 @@ def upload_page():
 def questions_page():
     file_path = st.session_state.get("file_path")
     if file_path is None or not os.path.exists(file_path):
-        st.error("ؤجاءًا رفع فايل الإكسل")
+        st.error("Please upload an Excel file first.")
         return
     
     # Read the Excel file data
-    data = pd.read_excel(file_path, engine="openpyxl")
-
+    data = pd.read_excel(file_path)
 
     # Function to save changes to the Excel file
     def save_changes_to_excel(data, file_path):
         try:
             data.to_excel(file_path, index=False)
-            st.success("تم حفظ التغييرات")
+            st.success("Changes Saved!")
         except Exception as e:
             st.error(f"An error occurred while saving changes: {e}")
 
@@ -60,20 +47,44 @@ def questions_page():
         st.error(f"The file '{file_path}' does not exist.")
     else:
         data = pd.read_excel(file_path)
+
+        # Define the CSS for styling
+        page_css = """
+            <style>
+                body {
+                    font-family: Arial, sans-serif;
+                }
+                .header {
+                    font-size: 24px;
+                    font-weight: bold;
+                    margin-bottom: 20px;
+                }
+                .question-container {
+                    background-color: #f5f5f5;
+                    border: 1px solid #ddd;
+                    padding: 20px;
+                    border-radius: 5px;
+                    margin-bottom: 20px;
+                    text-align: right; /* Align text to the right */
+                }
+                .button-container {
+                    margin-top: 10px;
+                }
+                .editable-answer {
+                    border: 1px solid #ddd;
+                    padding: 10px;
+                    border-radius: 5px;
+                }
+            </style>
+        """
+
+        # Add the CSS to the Streamlit app
+        st.markdown(page_css, unsafe_allow_html=True)
+
         # Page for displaying questions
         def page2(data):
             # Current question index
             current_question_idx = st.session_state.get("current_question_idx", 0)
-
-            # Dropdown to select question index number
-            question_indices = list(range(1, len(data) + 1))  # Assuming 1-based index
-            selected_question_idx = st.selectbox("Select Question Index:", question_indices, index=current_question_idx)
-
-            # Update the current_question_idx based on the selected index
-            current_question_idx = selected_question_idx - 1  # Adjust for 0-based index
-            st.session_state["current_question_idx"] = current_question_idx
-
-            # Display the question and choices with the selected question index
             course_name = data.iloc[current_question_idx]['Course Name']
             v_value = data.iloc[current_question_idx]['V']
             p_value = data.iloc[current_question_idx]['P']
@@ -82,26 +93,17 @@ def questions_page():
 
             # Header
             st.markdown(f"<h1 class='header'>{course_name}</h1>", unsafe_allow_html=True)
-            st.markdown(f"<h5 class='subheader'>الفيديو {v_value} في الفقرة {p_value}</div>",
+            st.markdown(f"<h5 class='subheader'>Video is: {v_value}, Paragraph is: {p_value}</div>",
                         unsafe_allow_html=True)
 
-            # Display the question and choices with the selected question index
+            # Display the question and choices with the question index
             question_container = st.container()
             with question_container:
                 question_idx = current_question_idx + 1  # Add 1 to make it 1-based index
-                # Calculate the height required for the text
-                question_text = data.iloc[current_question_idx]['Question']
-                text_height = len(question_text.split('\n')) * 25  # You can adjust the factor as needed
-                # Set the text-align property for the text_area to "right"
-                question = st.text_area(
-                    f"السؤال رقم {question_idx}",
-                    question_text,
-                    key=f"Question_{question_idx}",
-                    height=text_height  # Set the height based on the calculated height
-                )
+                question = st.text_area(f"Question {question_idx}:", data.iloc[current_question_idx]['Question'])
 
                 # Create two columns to display answer choices 1 & 2 in the same row and 3 & 4 in the same row
-                col1, col2 , col3 = st.columns(3)
+                col1, col2 = st.columns(2)
 
                 # Displaying answer choices 1 & 2 in the same row
                 with col1:
@@ -109,8 +111,7 @@ def questions_page():
                                not pd.isna(data.iloc[current_question_idx][f'Choice {i}'])]
                     edited_choices = []
                     for i, choice in enumerate(choices, start=1):
-                        # Set the text-align property for the text_input to "right"
-                        edited_choice = st.text_input(f"الإجابة رقم {i}", choice, key=f"Choice_{i}")
+                        edited_choice = st.text_input(f"الإجابة رقم {i}", choice)
                         edited_choices.append(edited_choice)
 
                 # Displaying answer choices 3 & 4 in the same row
@@ -119,34 +120,27 @@ def questions_page():
                                not pd.isna(data.iloc[current_question_idx][f'Choice {i}'])]
                     edited_choices += [None] * (4 - len(edited_choices))  # Ensure 'edited_choices' has at least 4 elements
                     for i, choice in enumerate(choices, start=3):
-                        # Set the text-align property for the text_input to "right"
-                        edited_choice = st.text_input(f"الإجابة رقم {i}", choice, key=f"Choice_{i}")
+                        edited_choice = st.text_input(f"الإجابة رقم {i}", choice)
                         edited_choices.append(edited_choice)
 
-                # Displaying answer choices 5 & 6 in the same row (if they exist)
-                with col3:
-                    for i in range(5, 7):
-                        choice_value = data.iloc[current_question_idx][f'Choice {i}']
-                        if not pd.isna(choice_value):
-                            # Set the text-align property for the text_input to " right"
-                            edited_choice = st.text_input(f"الإجابة رقم {i}", choice_value, key=f"Choice_{i}")
-                            edited_choices.append(edited_choice)
-
             # Display the correct answer (editable field)
-            correct_answer = st.text_input("الاختيار الصحيح", data.iloc[current_question_idx]['Right Answer'], key=f"Correct_Answer_{question_idx}")
+            correct_answer = st.text_input("الاختيار الصحيح", data.iloc[current_question_idx]['Right Answer'])
 
             # Button handlers
             button_container = st.container()
             with button_container:
-                col1, col2, col3, col4 = st.columns([10, 1, 2, 2])  # Adjust the column proportions as needed
-
-                if col1.button("السؤال السابق") and current_question_idx > 0:
+                col1, col2, col3 = st.columns(3)
+                if col1.button("Previous") and current_question_idx > 0:
                     with st.spinner("Loading..."):
                         current_question_idx -= 1
                         st.session_state["current_question_idx"] = current_question_idx
+                col2.empty()
+                if col3.button("Next") and current_question_idx < len(data) - 1:
+                    with st.spinner("Loading..."):
+                        current_question_idx += 1
+                        st.session_state["current_question_idx"] = current_question_idx
 
-                col2.empty()  # Empty column to create space
-                if col4.button("احفظ التعديلات"):
+                if col1.button("Save Changes"):
                     try:
                         # Update the DataFrame with edited question, answer choices, and correct answer
                         data.at[current_question_idx, 'Question'] = question
@@ -154,24 +148,18 @@ def questions_page():
                         data.at[current_question_idx, 'Choice 2'] = edited_choices[1]
                         data.at[current_question_idx, 'Choice 3'] = edited_choices[2]
                         data.at[current_question_idx, 'Choice 4'] = edited_choices[3]
-                        data.at[current_question_idx, 'Choice 5'] = edited_choices[4]
-                        data.at[current_question_idx, 'Choice 6'] = edited_choices[5]
                         data.at[current_question_idx, 'Right Answer'] = correct_answer
 
                         # Drop the "Choice 0" column if it exists
                         if 'Choice 0' in data.columns:
                             data = data.drop(columns=['Choice 0'])
 
+                        st.success("Changes saved successfully")
                     except Exception as e:
                         st.error(f"An error occurred: {str(e)}")
 
                     # Save all changes to the Excel file
                     save_changes_to_excel(data, file_path)
-
-                if col3.button("السؤال التالي") and current_question_idx < len(data) - 1:
-                    with st.spinner("Loading..."):
-                        current_question_idx += 1
-                        st.session_state["current_question_idx"] = current_question_idx
 
                 if current_question_idx == len(data) - 1:
                     st.markdown("<div class='header'>End of questions.</div>", unsafe_allow_html=True)
